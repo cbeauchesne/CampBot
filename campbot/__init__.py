9b579499c766d1d922a67d4bf86b7018b48a4169
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2018-07-19 00:22:31 +0200

    Moved to gitlab

--- campbot/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-campbot is a package for automatic edition of camptocamp.org
-"""
-
-from campbot.core import CampBot
-
--- campbot/utils.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-
-def today():
-    return datetime.today()
